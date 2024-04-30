de7fd248731bd5acf1fb67663b04d65775bbdea3
Jhon Alexander Acevedo <jhon.acevedo@inxeption.com>
2024-04-29 22:21:52 -0500

    init commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}", './node_modules/flowbite/**/*.js'],
+    theme: {
+        extend: {
+            colors: {
+                'node': '#539E43',
+                'angular': '#E23237',
+                'action': '#c27803',
+                'second-action': '#c27803'
+            }
+        },
+        screens: {
+            'sm': '576px',
+            'md': '960px',
+            'lg': '1440px',
+          },
+    },
+    plugins: [require('flowbite/plugin')],
+};
