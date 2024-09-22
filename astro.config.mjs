53a36cca6ef423b6eac667c5ecc3d882c82562f8
Jhon Acevedo <developer@jhonacevedo.com>
2024-09-21 23:30:53 -0500

    add internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,16 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  defaultLocale: "es",
+  locales: ["es", "en"],
+  routing: {
+      prefixDefaultLocale: true,
+
+  },
+  vite: {
+    json: {
+      namedExports: true, // This allows named exports from JSON files
+    },
+  },
 });
